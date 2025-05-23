25d176c942fc66b60b581af3f38bdca2ad9614f3
Alexandre Hallaine <alexandre@hallaine.com>
2025-05-23 11:10:11 -0400

    feat: move to component

---
+++ components/Wheel.vue
@@ -0,0 +1,87 @@
+<script lang="ts" setup>
+import * as d3 from 'd3'
+
+const wheel = useTemplateRef('wheel')
+const emit = defineEmits(['result'])
+const props = defineProps({
+  data: {
+    type: Array as () => { label: string, value: number }[],
+    required: true
+  },
+  angle: {
+    type: Number,
+    required: true
+  },
+  size: {
+    type: Number,
+    default: 400
+  },
+  time: {
+    type: Number,
+    default: 3000
+  }
+})
+
+const arcs = d3.pie().value(d => d.value)(props.data)
+const arc = d3.arc().innerRadius(0).outerRadius(props.size / 2)
+const color = d3.scaleOrdinal(d3.schemeSet2)
+
+let g, texts
+onMounted(() => {
+  const svg = d3.select(wheel.value)
+      .append('svg')
+      .attr('width', props.size)
+      .attr('height', props.size)
+
+  g = svg.append('g')
+      .attr('transform', `translate(${props.size / 2},${props.size / 2})`)
+      .attr('id', 'wheel')
+
+  g.selectAll('path')
+      .data(arcs)
+      .enter()
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', d => color(d.data.label))
+
+  texts = g.selectAll('text')
+      .data(arcs)
+      .enter()
+      .append('text')
+      .attr('transform', d => {
+        const [x, y] = arc.centroid(d)
+        return `translate(${x},${y})`
+      })
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'middle')
+      .text(d => d.data.label)
+})
+
+watch(toRef(props, 'angle'), (newAngle, oldAngle) => {
+  const i = d3.interpolate(oldAngle, newAngle)
+
+  g.transition()
+      .duration(props.time)
+      .ease(d3.easeCubicOut)
+      .attrTween('transform', () => t => `translate(${props.size / 2},${props.size / 2}) rotate(${i(t)})`)
+
+  texts.transition()
+      .duration(props.time)
+      .ease(d3.easeCubicOut)
+      .attrTween('transform', d => {
+        const [x, y] = arc.centroid(d)
+        return t => `translate(${x},${y}) rotate(${-i(t)})`
+      })
+
+  setTimeout(() => {
+    const finalAngle = newAngle % 360
+    const segmentAngle = 360 / props.data.length
+    const index = Math.floor((360 - finalAngle + segmentAngle / 2) % 360 / segmentAngle)
+    emit('result', props.data[index]?.label)
+  }, props.time)
+})
+</script>
+
+<template>
+  <div ref="wheel"/>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,29 @@
 <script setup>
-import * as d3 from 'd3'
-
-const wheel = useTemplateRef('wheel')
-const prize = ref('No prize')
-const angle = ref(0)
 const data = [
   { label: 'Alex', value: 40 },
   { label: 'Bob', value: 25 },
   { label: 'Charlie', value: 35 }
 ]
 
-const size = 400
-const time = 3000
-const arcs = d3.pie().value(d => d.value)(data)
-const arc = d3.arc().innerRadius(0).outerRadius(size / 2)
-const color = d3.scaleOrdinal(d3.schemeSet2)
-
-onMounted(() => {
-  const svg = d3.select(wheel.value).append('svg').attr('width', size).attr('height', size)
-  const outer = svg.append('g').attr('transform', `translate(${size / 2},${size / 2})`)
-  const g = outer.append('g').attr('id', 'wheel')
-
-  g.selectAll('path')
-    .data(arcs)
-    .enter()
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', d => color(d.data.label))
-
-  const textGroups = g.selectAll('g.label')
-    .data(arcs)
-    .enter()
-    .append('g')
-    .attr('class', 'label')
-    .attr('transform', d => `translate(${arc.centroid(d)})`)
-
-  textGroups.append('text')
-    .text(d => d.data.label)
-    .attr('text-anchor', 'middle')
-    .attr('alignment-baseline', 'middle')
-
-  watch(angle, (newAngle, oldAngle) => {
-    const i = d3.interpolate(oldAngle, newAngle)
-
-    g.transition()
-      .duration(time)
-      .ease(d3.easeCubicOut)
-      .attrTween("transform", () => t => `rotate(${i(t)})`)
-
-    textGroups.select('text')
-      .transition()
-      .duration(time)
-      .ease(d3.easeCubicOut)
-      .attrTween('transform', () => t => `rotate(${-i(t)})`)
-  })
-})
+const angle = ref(0)
+const prize = ref('No prize')
 
 function spin() {
-    angle.value += Math.random() * 10 * 360
-
-    setTimeout(() => {
-      const finalAngle = angle.value % 360
-      const segmentAngle = 360 / data.length
-      const index = Math.floor((360 - finalAngle + 90) % 360 / segmentAngle)
-
-      prize.value = data[index]?.label ?? 'No prize'
-    }, time)
+  angle.value += 5 * 360 + Math.random() * 5 * 360
 }
-
-setInterval(() => spin(), 5000)
 </script>
-
 
 <template>
   <UPageSection>
-    <UPageCTA title="D3 Wheel" orientation="horizontal" :description="`Prize: ${ prize }`">
-      <div ref="wheel" />
+    <UPageCTA
+        :description="`Prize: ${prize}`"
+        orientation="horizontal"
+        title="D3 Wheel"
+    >
+      <template #links>
+        <UButton label="Spin the Wheel" @click="spin"/>
+      </template>
+      <Wheel :angle="angle" :data="data" @result="result => prize = result"/>
     </UPageCTA>
   </UPageSection>
 </template>
